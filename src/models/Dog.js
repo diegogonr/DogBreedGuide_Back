60e817aa6ea801ca16d5d55c6d7976a6c3051d0c
DgonzalesR30 <gonzalesrodriguezdiego@gmail.com>
2023-11-22 13:59:28 -0500

    update

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -1,4 +1,4 @@
-const { DataTypes } = require('sequelize');
+const {Sequelize, DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
@@ -8,7 +8,6 @@ module.exports = (sequelize) => {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
-      defaultValue: Sequelize.literal('265'),
     },
     img: {
       type: DataTypes.STRING,
